61ed3c85e31f9cc393fa7191083f74ece64fc8b5
RSatanovskyi <satcms@hotmail.com>
2019-12-26 11:53:05 +0200

    entity framework

---
+++ EFCoreDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+namespace EFCoreDemo
+{
+    class Program
+    {
+        static void Main2(string[] args)
+        {
+            using (var context = new EFCoreDemoContext())
+            {
+                var author = new Author
+                {
+                    FirstName = "William",
+                    LastName = "Shakespeare",
+                    Books = new List<Book>
+                    {
+                        new Book { Title = "Hamlet"},
+                        new Book { Title = "Othello" },
+                        new Book { Title = "MacBeth" }
+                    }
+                };
+                context.Add(author);
+                context.SaveChanges();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            using (var context = new EFCoreDemoContext())
+            {
+                foreach (var book in context.Books)
+                {
+                    System.Console.WriteLine(book.Title);
+                }
+                System.Console.ReadLine();
+            }
+        }
+    }
+}
